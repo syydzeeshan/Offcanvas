b56575e7807704b6b014b14fcfc80a204bd1cd35
Zeeshan Ali Sayyad <zeeshan.s@agaetis.tech>
2023-05-23 07:17:24 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function openNav() {
+  document.querySelector(".sidenav").style.width = "25%";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.querySelector(".sidenav").style.width = "0";
+}
